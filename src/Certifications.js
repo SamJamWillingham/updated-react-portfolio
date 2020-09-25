77036ec4d2b3fbc6c6369ec64baa9eb61b52ddd4
Sam Willingham <68390031+SamJamWillingham@users.noreply.github.com>
2020-09-25 10:44:20 -0400

    added new certification to Certifications compo.

--- src/Certifications.js
+++ src/Certifications.js
@@ -21,13 +21,19 @@ export default function Certifications() {
           id="certImg"
         />
       </a>
-      <a href="https://www.freecodecamp.org/certification/samjamwillingham/responsive-web-design">
+      <a href="https://www.shecodes.io/certificates/370d835ecda3fd24895ade35bcb679ed">
         <img
-          src="https://lh3.googleusercontent.com/hoTMjsF-Xpjbx-NVC61eQBKFTln1hIglqlKg3g4rmLJbSVv2PR-5LyD0r1o6gDf17CCd5r0DpL5-IdLLN4pIrAdQ6wcs7slR0WTwLgbTcrvdOyy9xxePwoaIQVgXbqfrH-2aI8jF5PIzhvZ8EgNOlqsuJ4rjMmI7FCiG8fYmVyAFaHSOyWvGtS9wJViUx---m47Kh7zCBrulxTfSRGkeZrOWXGlsYVy7wUG11z5hSDll-tahIh9eyq5j58EP5Muy-IYxCKPbX_Bo-l-X0uXSenEg8qSxc-dYCBM81XwOhxqn_dWqoR7WUO61iXfmL4gDx0bxfZnb-3nSifslgD41Q_A07a3OTGh5OlhpnENAjb1zZK1fBqyo_K2mMBFP0n08ZDDI-yR5H3uB68n99lpYlaNOvZs57Otn4cUrCIgxsfDz_f2qil4snkbDJ2VkNHgDQtIfS7GEOChRk478nyIFEA_NJMF4pWwzlnYLUT6EScNy5SxeDI2qXM032AQyRmj6sW6CfKzx6B6iU5NjBdmYE-ygqd_1TAAcnekwmPSLHsvAxnmnxe6NKG7TrlNSCCmHgYLsOq-lS-ezvMea2fK7wikZBxs-iYjfBwfXbfl5TAdHGCP7yIkyzYE5hdgWQ3VWl30bD3-L-WmUigfGEVk8IxZ-18NoA3zGMkbe8wpUp8DOjA94iTbnnPsH0FXFbw=w1047-h625-no?authuser=0"
-          alt="Responsive Web Design"
+          src="https://s3.amazonaws.com/shecodesio-production/students/certificates/000/011/119/original/result.png?1601044095"
+          alt="React Development"
           width={300}
           id="certImg"
         />
+      </a>
+      <a href="https://www.freecodecamp.org/certification/samjamwillingham/responsive-web-design">
+        <button>
+          <i class="fab fa-free-code-camp"></i>
+          view cetifications in freeCodeCamp
+        </button>
       </a>
     </section>
   );
