76a62a9078f2c3bce407d48719bdcecc9dc88a74
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-10-09 22:39:13 -0700

    Removed bg rules and wrapped projects in the container

--- src/Projects.js
+++ src/Projects.js
@@ -9,84 +9,94 @@ export default function Projects() {
       <div className="bg"></div>
       <div className="bg bg2"></div>
       <div className="bg bg3"></div>
-      <h3>Projects</h3>
-      <div className="project-flex">
-        <div className="card">
-          <i className="fas fa-cloud-sun"></i>
-          <h4>Vanilla JS Weather App</h4>
-          <p>
-            I created this app during the SheCodes Front End Development
-            workshop. It uses vanilla javascript to call an OpenWeather API for
-            current weather data. Styling was done with CSS and Bootstrap.
-          </p>
-          <ViewProjectBtns
-            link="https://gifted-tereshkova-4aa25e.netlify.app/"
-            buttonName="App"
-          />
-          <ViewProjectBtns
-            link="https://github.com/SamJamWillingham/Weather-App-SheCodesPlus"
-            buttonName="Code"
-          />
+      <div id="projects-container">
+        <h3>Projects</h3>
+        <div className="project-flex">
+          <div className="card">
+            <i className="fas fa-cloud-sun"></i>
+            <h2>Vanilla JS Weather App</h2>
+            <p>
+              I created this app during the SheCodes Front End Development
+              workshop. It uses vanilla javascript to call an OpenWeather API for
+              current weather data. Styling was done with CSS and Bootstrap.
+            </p>
+            <div id="project-button-container">
+              <ViewProjectBtns
+                link="https://gifted-tereshkova-4aa25e.netlify.app/"
+                buttonName="App"
+              />
+              <ViewProjectBtns
+                link="https://github.com/SamJamWillingham/Weather-App-SheCodesPlus"
+                buttonName="Code"
+              />
+            </div>
+          </div>
+          <div className="card">
+            <i className="fas fa-hand-sparkles"></i>
+            <h2>Tarot Card Drawer</h2>
+            <p>
+              I created this small widget as a side project for fun to practice
+              HTML, CSS, and JS. It uses vanilla javascript to select a random
+              card and show you an interpretation of the cards meaning and it's
+              advice.
+            </p>
+            <div id="project-button-container">
+              <ViewProjectBtns
+                link="https://ecstatic-leakey-e874b0.netlify.app/"
+                buttonName="App"
+              />
+              <ViewProjectBtns
+                link="https://github.com/SamJamWillingham/Tarot-Card-Drawer"
+                buttonName="Code"
+              />
+            </div>
+          </div>
+          <div className="card">
+            <i className="fab fa-react projects"></i>
+            <h2>React Weather App</h2>
+            <p>
+              I created this app during the SheCodes React Development workshop.
+              It uses React and JSX to call an API for current weather data.
+              Animated styling was done with CSS and React-Bootstrap.
+            </p>
+            <div id="project-button-container">
+              <ViewProjectBtns
+                link="https://clever-hugle-333d69.netlify.app/"
+                buttonName="App"
+              />
+              <ViewProjectBtns
+                link="https://github.com/SamJamWillingham/react-weather-app"
+                buttonName="Code"
+              />
+            </div>
+          </div>
+          <div className="card">
+            <i className="fas fa-om"></i>
+            <h2>Meditation App</h2>
+            <p>
+              I created this Meditation timer with Dev Ed. It uses vanilla
+              JavaScript to animate a timer, count down different set amounts of
+              time, and change the audio track between two different ambient
+              sounds.
+            </p>
+            <div id="project-button-container">
+              <ViewProjectBtns
+                link="https://lucid-clarke-3b55e8.netlify.app/"
+                buttonName="App"
+              />
+              <ViewProjectBtns
+                link="https://github.com/SamJamWillingham/Meditation-App"
+                buttonName="Code"
+              />
+            </div>
+          </div>
         </div>
-        <div className="card">
-          <i className="fas fa-hand-sparkles"></i>
-          <h4>Tarot Card Drawer</h4>
-          <p>
-            I created this small widget as a side project for fun to practice
-            HTML, CSS, and JS. It uses vanilla javascript to select a random
-            card and show you an interpretation of the cards meaning and it's
-            advice.
-          </p>
-          <ViewProjectBtns
-            link="https://ecstatic-leakey-e874b0.netlify.app/"
-            buttonName="App"
-          />
-          <ViewProjectBtns
-            link="https://github.com/SamJamWillingham/Tarot-Card-Drawer"
-            buttonName="Code"
-          />
-        </div>
-        <div className="card">
-          <i className="fab fa-react projects"></i>
-          <h4>React Weather App</h4>
-          <p>
-            I created this app during the SheCodes React Development workshop.
-            It uses React and JSX to call an API for current weather data.
-            Animated styling was done with CSS and React-Bootstrap.
-          </p>
-          <ViewProjectBtns
-            link="https://clever-hugle-333d69.netlify.app/"
-            buttonName="App"
-          />
-          <ViewProjectBtns
-            link="https://github.com/SamJamWillingham/react-weather-app"
-            buttonName="Code"
-          />
-        </div>
-        <div className="card">
-          <i className="fas fa-om"></i>
-          <h4>Meditation App</h4>
-          <p>
-            I created this Meditation timer with Dev Ed. It uses vanilla
-            JavaScript to animate a timer, count down different set amounts of
-            time, and change the audio track between two different ambient
-            sounds.
-          </p>
-          <ViewProjectBtns
-            link="https://lucid-clarke-3b55e8.netlify.app/"
-            buttonName="App"
-          />
-          <ViewProjectBtns
-            link="https://github.com/SamJamWillingham/Meditation-App"
-            buttonName="Code"
-          />
-        </div>
+        <ViewProjectBtns
+          link="https://dazzling-perlman-5655fe.netlify.app/"
+          buttonName="Want to see more projects? Check out my progress on the Javascript
+              30 Day Challenge from Wes Bos!"
+        />
       </div>
-      <ViewProjectBtns
-        link="https://dazzling-perlman-5655fe.netlify.app/"
-        buttonName="Want to see more projects? Check out my progress on the Javascript
-            30 Day Challenge from Wes Bos!"
-      />
     </div>
   );
 }
