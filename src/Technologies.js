0143faa61f97a6604a59635358669598cfe6ffaa
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-10-09 22:41:19 -0700

    Wrapped tehnologies into paragraph and added scale for smaller screens

--- src/Technologies.js
+++ src/Technologies.js
@@ -11,63 +11,81 @@ export default function Technologies() {
           <i className="fab fa-html5" aria-hidden="false"></i>
         </div>
 
-        <div className="ribbon">HTML</div>
+        <div className="ribbon">
+          <p>HTML</p>
+        </div>
       </div>
 
       <div className="badge blue-dark">
         <div className="circle">
           <i className="fab fa-css3-alt" aria-hidden="false"></i>
         </div>
-        <div className="ribbon">CSS3</div>
+        <div className="ribbon">
+          <p>CSS3</p>
+        </div>
       </div>
 
       <div className="badge orange">
         <div className="circle">
           <i className="fab fa-js-square" aria-hidden="false"></i>
         </div>
-        <div className="ribbon">ES6</div>
+        <div className="ribbon">
+          <p>ES6</p>
+        </div>
       </div>
 
       <div className="badge teal">
         <div className="circle">
           <i className="fas fa-cog" aria-hidden="false"></i>
         </div>
-        <div className="ribbon">API</div>
+        <div className="ribbon">
+          <p>API</p>
+        </div>
       </div>
 
       <div className="badge purple">
         <div className="circle">
           <i className="fab fa-bootstrap" aria-hidden="false"></i>
         </div>
-        <div className="ribbon">Bootstrap</div>
+        <div className="ribbon">
+          <p>Bootstrap</p>
+        </div>
       </div>
 
       <div className="badge blue">
         <div className="circle">
           <i className="fab fa-react" aria-hidden="false"></i>
         </div>
-        <div className="ribbon">React</div>
+        <div className="ribbon">
+          <p>React</p>
+        </div>
       </div>
 
       <div className="badge silver">
         <div className="circle">
           <i className="fas fa-code" aria-hidden="false"></i>
         </div>
-        <div className="ribbon">VS Code</div>
+        <div className="ribbon">
+          <p>VS Code</p>
+        </div>
       </div>
 
       <div className="badge red">
         <div className="circle">
           <i className="fab fa-codepen" aria-hidden="false"></i>
         </div>
-        <div className="ribbon">Code Pen</div>
+        <div className="ribbon">
+          <p>Code Pen</p>
+        </div>
       </div>
 
       <div className="badge pink">
         <div className="circle">
           <i className="fab fa-github" aria-hidden="false"></i>
         </div>
-        <div className="ribbon">GitHub</div>
+        <div className="ribbon">
+          <p>GitHub</p>
+        </div>
       </div>
     </section>
   );
