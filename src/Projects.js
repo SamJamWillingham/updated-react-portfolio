cc936f0966c3080d5528a63ea426c6e6f8303501
Sam Willingham <68390031+SamJamWillingham@users.noreply.github.com>
2020-10-12 10:54:10 -0400

    new fonts, images, colors; added animation to project cards

--- src/Projects.js
+++ src/Projects.js
@@ -7,96 +7,73 @@ import ViewProjectBtns from "./ViewProjectBtns";
 export default function Projects() {
   return (
     <div id="projects">
-      <div className="bg"></div>
-      <div className="bg bg2"></div>
-      <div className="bg bg3"></div>
-      <div id="projects-container">
-        <h2>Projects</h2>
-        <div className="project-flex">
-          <div className="card">
-            <i className="fas fa-cloud-sun"></i>
-            <h3>Vanilla JS Weather App</h3>
-            <p>
-              I created this app during the SheCodes Front End Development
-              workshop. It uses vanilla javascript to call an OpenWeather API for
-              current weather data. Styling was done with CSS and Bootstrap.
-            </p>
-            <div id="project-button-container">
-              <ViewProjectBtns
-                link="https://gifted-tereshkova-4aa25e.netlify.app/"
-                buttonName="App"
-              />
-              <ViewProjectBtns
-                link="https://github.com/SamJamWillingham/Weather-App-SheCodesPlus"
-                buttonName="Code"
-              />
+      <div className="bg2">
+        <div id="projects-container">
+          <h2>Projects</h2>
+          <div className="project-flex">
+            <div className="card">
+              <i className="fas fa-cloud-sun"></i>
+              <h3>Vanilla JS Weather App</h3>
+              <div id="project-button-container">
+                <ViewProjectBtns
+                  link="https://gifted-tereshkova-4aa25e.netlify.app/"
+                  buttonName="app"
+                />
+                <ViewProjectBtns
+                  link="https://github.com/SamJamWillingham/Weather-App-SheCodesPlus"
+                  buttonName="code"
+                />
+              </div>
+            </div>
+            <div className="card">
+              <i className="fas fa-hand-sparkles"></i>
+              <h3>Tarot Card Drawer</h3>
+              <div id="project-button-container">
+                <ViewProjectBtns
+                  link="https://ecstatic-leakey-e874b0.netlify.app/"
+                  buttonName="app"
+                />
+                <ViewProjectBtns
+                  link="https://github.com/SamJamWillingham/Tarot-Card-Drawer"
+                  buttonName="code"
+                />
+              </div>
+            </div>
+            <div className="card">
+              <i className="fab fa-react projects"></i>
+              <h3>React Weather App</h3>
+              <div id="project-button-container">
+                <ViewProjectBtns
+                  link="https://clever-hugle-333d69.netlify.app/"
+                  buttonName="app"
+                />
+                <ViewProjectBtns
+                  link="https://github.com/SamJamWillingham/react-weather-app"
+                  buttonName="code"
+                />
+              </div>
+            </div>
+            <div className="card">
+              <i className="fas fa-om"></i>
+              <h3>Meditation App</h3>
+              <div id="project-button-container">
+                <ViewProjectBtns
+                  link="https://lucid-clarke-3b55e8.netlify.app/"
+                  buttonName="app"
+                />
+                <ViewProjectBtns
+                  link="https://github.com/SamJamWillingham/Meditation-App"
+                  buttonName="code"
+                />
+              </div>
             </div>
           </div>
-          <div className="card">
-            <i className="fas fa-hand-sparkles"></i>
-            <h3>Tarot Card Drawer</h3>
-            <p>
-              I created this small widget as a side project for fun to practice
-              HTML, CSS, and JS. It uses vanilla javascript to select a random
-              card and show you an interpretation of the cards meaning and it's
-              advice.
-            </p>
-            <div id="project-button-container">
-              <ViewProjectBtns
-                link="https://ecstatic-leakey-e874b0.netlify.app/"
-                buttonName="App"
-              />
-              <ViewProjectBtns
-                link="https://github.com/SamJamWillingham/Tarot-Card-Drawer"
-                buttonName="Code"
-              />
-            </div>
-          </div>
-          <div className="card">
-            <i className="fab fa-react projects"></i>
-            <h3>React Weather App</h3>
-            <p>
-              I created this app during the SheCodes React Development workshop.
-              It uses React and JSX to call an API for current weather data.
-              Animated styling was done with CSS and React-Bootstrap.
-            </p>
-            <div id="project-button-container">
-              <ViewProjectBtns
-                link="https://clever-hugle-333d69.netlify.app/"
-                buttonName="App"
-              />
-              <ViewProjectBtns
-                link="https://github.com/SamJamWillingham/react-weather-app"
-                buttonName="Code"
-              />
-            </div>
-          </div>
-          <div className="card">
-            <i className="fas fa-om"></i>
-            <h3>Meditation App</h3>
-            <p>
-              I created this Meditation timer with Dev Ed. It uses vanilla
-              JavaScript to animate a timer, count down different set amounts of
-              time, and change the audio track between two different ambient
-              sounds.
-            </p>
-            <div id="project-button-container">
-              <ViewProjectBtns
-                link="https://lucid-clarke-3b55e8.netlify.app/"
-                buttonName="App"
-              />
-              <ViewProjectBtns
-                link="https://github.com/SamJamWillingham/Meditation-App"
-                buttonName="Code"
-              />
-            </div>
-          </div>
-        </div>
-        <ViewProjectBtns
-          link="https://dazzling-perlman-5655fe.netlify.app/"
-          buttonName="Want to see more projects? Check out my progress on the Javascript
+          <ViewProjectBtns
+            link="https://dazzling-perlman-5655fe.netlify.app/"
+            buttonName="Want to see more projects? Check out my progress on the Javascript
               30 Day Challenge from Wes Bos!"
-        />
+          />
+        </div>
       </div>
     </div>
   );
--- src/Welcome.js
+++ src/Welcome.js
@@ -5,60 +5,56 @@ import "./BackgroundAnimation.css";
 export default function Welcome() {
   return (
     <div id="welcome-section" className="flexbox-container">
-      <div className="bg"></div>
-      <div className="bg bg2"></div>
-      <div className="bg bg3"></div>
-
-      <div className="profile flexbox-item flexbox-item-2">
-        <div id="name">
-          <h1>Sam Willingham</h1>
-          <div id="title-location">
-            <h3>
-              Front End Web Developer
-            </h3>
-            <h3>Charlotte, NC, US</h3>
+      <div className="bg">
+        <div className="flexbox-item-2">
+          <div id="name">
+            <h1>Sam Willingham</h1>
+            <div id="title-location">
+              <h3>Front End Web Developer</h3>
+              <h3>Charlotte, NC, US</h3>
+            </div>
           </div>
-        </div>
 
-        <div id="button-container">
-        <a
-          href="https://www.linkedin.com/in/samantha-willingham-79576a1a8/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button className="social">
-            <i className="fab fa-linkedin-in"></i>
-          </button>
-        </a>
+          <div id="button-container">
+            <a
+              href="https://www.linkedin.com/in/samantha-willingham-79576a1a8/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="social">
+                <i className="fab fa-linkedin-in"></i>
+              </button>
+            </a>
 
-        <a
-          href="https://github.com/SamJamWillingham"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button className="social">
-            <i className="fab fa-github"></i>
-          </button>
-        </a>
+            <a
+              href="https://github.com/SamJamWillingham"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="social">
+                <i className="fab fa-github"></i>
+              </button>
+            </a>
 
-        <a
-          href="https://www.shecodes.io/students/174-sam-fields"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button className="social">
-            <i className="fas fa-laptop-code"></i>
-          </button>
-        </a>
-        <a
-          href="https://www.freecodecamp.org/samjamwillingham"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button className="social">
-            <i className="fab fa-free-code-camp"></i>
-          </button>
-        </a>
+            <a
+              href="https://www.shecodes.io/students/174-sam-fields"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="social">
+                <i className="fas fa-laptop-code"></i>
+              </button>
+            </a>
+            <a
+              href="https://www.freecodecamp.org/samjamwillingham"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="social">
+                <i className="fab fa-free-code-camp"></i>
+              </button>
+            </a>
+          </div>
         </div>
       </div>
     </div>
