992bf0b8e83283d3812a519b8bd7ef20dc5d8115
Sam Willingham <68390031+SamJamWillingham@users.noreply.github.com>
2020-10-19 09:52:02 -0400

    added hidden Spotify playlist that pops up with a Konami Code

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from "react";
 import "./App.css";
 import Navbar from "./Navbar.js";
 import Welcome from "./Welcome";
+import HiddenPlaylist from "./HiddenPlaylist";
 import Technologies from "./Technologies";
 import Projects from "./Projects";
 import Certifications from "./Certifications";
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Welcome />
+      <HiddenPlaylist />
       <Technologies />
       <Projects />
       <Certifications />
---
+++ src/HiddenPlaylist.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import "./App.css";
+
+export default function HiddenPlaylist() {
+    const pressed = [];
+const secretCode = 'samjam';
+let [playlist, setPlaylist] = useState(false);
+
+window.addEventListener("keyup", (e) => {
+  console.log(e.key);
+  pressed.push(e.key);
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+
+  if (pressed.join('').includes(secretCode)) {
+    console.log('You found my hidden playlist! Nice!')
+   setPlaylist(true);
+  }
+});
+if(playlist) {
+ return(<div>
+   <div className="playlist-wrapper"></div>
+   <h2>You found my secret playlist! Nice!</h2>
+       <iframe src="https://open.spotify.com/embed/album/4uG8q3GPuWHQlRbswMIRS6?si=HvL3B0KJTe6DeYggflcXAw" width="300" height="380" frameBorder="0"
+    allowtransparency="true" allow="encrypted-media" className="playlist"></iframe>
+    </div>);
+} else {
+  return <div></div>
+}
+    
+} 
