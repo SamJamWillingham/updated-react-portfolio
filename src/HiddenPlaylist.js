061f9fc98f2295891fe33ec6f58ad4ce3278a1bc
Sam Willingham <68390031+SamJamWillingham@users.noreply.github.com>
2020-10-19 10:04:40 -0400

    added title property to iframe playlist

--- src/HiddenPlaylist.js
+++ src/HiddenPlaylist.js
@@ -21,7 +21,7 @@ if(playlist) {
    <div className="playlist-wrapper"></div>
    <h2>You found my secret playlist! Nice!</h2>
        <iframe src="https://open.spotify.com/embed/album/4uG8q3GPuWHQlRbswMIRS6?si=HvL3B0KJTe6DeYggflcXAw" width="300" height="380" frameBorder="0"
-    allowtransparency="true" allow="encrypted-media" className="playlist"></iframe>
+    allowtransparency="true" allow="encrypted-media" className="playlist" title="hidden-playlist"></iframe>
     </div>);
 } else {
   return <div></div>
